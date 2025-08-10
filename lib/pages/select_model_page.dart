5c4ae127f119a490b093858e75c742ee4bc571cc
leul <leulwebsdev@gmail.com>
2025-08-10 10:47:22 +0300

    scanner list page ui

--- lib/pages/select_model_page.dart
+++ lib/pages/select_model_page.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:go_router/go_router.dart';
 import 'package:grow_right_mobile/data/scanner_list.dart';
 import 'package:grow_right_mobile/widgets/model_selector_widget.dart';
+import 'package:grow_right_mobile/widgets/padding_wrapper.dart';
 
 class SelectModelPage extends StatelessWidget {
   const SelectModelPage({super.key});
@@ -10,38 +12,58 @@ class SelectModelPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Look For Crop Disease")),
-      body: Column(
-        children: [
-          ModelSelectorWidget(
-            title: "Coffee",
-            description: "Cereal Crop disease detection",
-            diseases: 3,
-            iconPath: "assets/images/coffee.svg",
-            action: () {
-              context.push("/inputScan", extra: scannerList[0]);
-            },
+      body: PaddingWrapper(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              const SizedBox(height: 12),
+              CircleAvatar(
+                radius: 40,
+                backgroundColor: Theme.of(context).primaryColor.withAlpha(40),
+                child: SvgPicture.asset(
+                  "assets/images/plant.svg",
+                  width: 40,
+                  color: Theme.of(context).primaryColor,
+                ),
+              ),
+              const SizedBox(height: 12),
+              Text(
+                "Choose the type of crop you want to analyze for disease detection",
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 12),
+              ModelSelectorWidget(
+                title: "Coffee",
+                description: "Cereal Crop disease detection",
+                diseases: 3,
+                iconPath: "assets/images/coffee.svg",
+                action: () {
+                  context.push("/inputScan", extra: scannerList[0]);
+                },
+              ),
+              const SizedBox(height: 12),
+              ModelSelectorWidget(
+                title: "Mango",
+                description: "Tropical fruit disease detection",
+                diseases: 3,
+                iconPath: "assets/images/mango.svg",
+                action: () {
+                  context.push("/inputScan", extra: scannerList[1]);
+                },
+              ),
+              const SizedBox(height: 12),
+              ModelSelectorWidget(
+                title: "Wheat",
+                description: "Cereal Crop disease detection",
+                diseases: 2,
+                iconPath: "assets/images/wheat.svg",
+                action: () {
+                  context.push("/inputScan", extra: scannerList[2]);
+                },
+              ),
+            ],
           ),
-          const SizedBox(height: 12),
-          ModelSelectorWidget(
-            title: "Mango",
-            description: "Tropical fruit disease detection",
-            diseases: 3,
-            iconPath: "assets/images/mango.svg",
-            action: () {
-              context.push("/inputScan", extra: scannerList[1]);
-            },
-          ),
-          const SizedBox(height: 12),
-          ModelSelectorWidget(
-            title: "Wheat",
-            description: "Cereal Crop disease detection",
-            diseases: 2,
-            iconPath: "assets/images/wheat.svg",
-            action: () {
-              context.push("/inputScan", extra: scannerList[2]);
-            },
-          ),
-        ],
+        ),
       ),
     );
   }
--- lib/widgets/model_selector_widget.dart
+++ lib/widgets/model_selector_widget.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter_svg/svg.dart';
 
 class ModelSelectorWidget extends StatelessWidget {
   final String title;
@@ -7,13 +7,14 @@ class ModelSelectorWidget extends StatelessWidget {
   final String iconPath;
   final int diseases;
   final VoidCallback action;
+
   const ModelSelectorWidget({
     super.key,
     required this.title,
     required this.description,
     required this.diseases,
     required this.iconPath,
-    required this.action
+    required this.action,
   });
 
   @override
@@ -21,30 +22,64 @@ class ModelSelectorWidget extends StatelessWidget {
     return GestureDetector(
       onTap: action,
       child: Container(
+        width: double.infinity, // ensures bounded width if inside Column
+        padding: const EdgeInsets.all(10),
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(12),
-          border: BoxBorder.all(color: Colors.grey[200]!, width: 1),
+          border: Border.all(color: Colors.grey[200]!, width: 1),
         ),
         child: Row(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            SvgPicture.asset(iconPath, width: 24),
-            const SizedBox(width: 10),
-            Column(
-              children: [
-                Text(
-                  title,
-                  style: Theme.of(context).textTheme.titleMedium!.copyWith(
-                    color: Theme.of(context).primaryColor,
-                  ),
-                ),
-                Text(description),
-                Chip(
-                  label: Text("Common diseases $diseases"),
-                  backgroundColor: Theme.of(context).primaryColor,
-                ),
-              ],
+            CircleAvatar(
+              backgroundColor: Theme.of(context).primaryColor.withAlpha(50),
+              child: SvgPicture.asset(iconPath, width: 24),
             ),
+            const SizedBox(width: 20),
+
+            // Make the text area take remaining width
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    title,
+                    style: Theme.of(context).textTheme.titleMedium!.copyWith(
+                      color: Theme.of(context).primaryColor,
+                    ),
+                  ),
+                  // No inner Row needed
+                  Text(
+                    description,
+                    softWrap: true,
+                    // optional:
+                    // maxLines: 3,
+                    // overflow: TextOverflow.ellipsis,
+                  ),
+                  const SizedBox(height: 12),
+                  Wrap(
+                    spacing: 8,
+                    runSpacing: 8,
+                    children: [
+                      Chip(
+                        label: Text(
+                          "Common diseases $diseases",
+                          style: Theme.of(context).textTheme.bodySmall!
+                              .copyWith(color: Theme.of(context).primaryColor),
+                        ),
+                        shape: const StadiumBorder(),
+                        backgroundColor: Theme.of(
+                          context,
+                        ).primaryColor.withAlpha(50),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+
+            const SizedBox(width: 12),
+            const Icon(Icons.arrow_forward),
           ],
         ),
       ),
